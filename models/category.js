acdc63a576ba2ad37308081ef490a26708504211
Sam <re711kimo@gmail.com>
2020-05-21 11:17:50 +0800

    feat: delete models/category.js & models/seeds/category.json & models/seeds/expense.json

--- models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const categorySchema = new Schema({
-  category: {
-    type: String,
-    required: true
-  }
-})
-module.exports = mongoose.model('Category', categorySchema)
