be4f7e01831d909787c51d8d204082df6c5a360f
Sam <re711kimo@gmail.com>
2020-05-22 14:08:06 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -85,6 +85,15 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 刪除功能
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
